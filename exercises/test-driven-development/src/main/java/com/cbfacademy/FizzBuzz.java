8a7f689b29ad0218eecec85d2b469ba2d61fac51
Hani Ali <haniali1204@gmail.com>
2024-08-20 21:35:31 +0100

    TDD -FizzBuzz exercise

--- exercises/test-driven-development/src/main/java/com/cbfacademy/FizzBuzz.java
+++ exercises/test-driven-development/src/main/java/com/cbfacademy/FizzBuzz.java
@@ -1,5 +1,16 @@
 package com.cbfacademy;
 
 public class FizzBuzz {
-    
+    public static String get(Integer i){
+        if(i % 15 == 0){
+            return "FizzBuzz";
+        }
+        else if(i % 5 == 0){
+            return "Buzz";
+        } else if(i % 3 == 0){
+            return "Fizz";
+        } else
+        return i.toString();
+    }
 }
+
--- exercises/test-driven-development/src/test/java/com/cbfacademy/AppTest.java
+++ exercises/test-driven-development/src/test/java/com/cbfacademy/AppTest.java
@@ -1,8 +1,11 @@
 package com.cbfacademy;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -17,4 +20,22 @@ public class AppTest {
 
         assertThat(app, is(notNullValue()));
     }
+    @Test
+    @DisplayName("prints Fizz")
+    public void testFizz(){
+        assertEquals("Fizz", FizzBuzz.get(3));
+        assertEquals("Fizz", FizzBuzz.get(6));
+    }
+    @Test
+    @DisplayName("prints Buzz")
+    public void testBuzz(){
+        assertEquals("Buzz", FizzBuzz.get(5));
+        assertEquals("Buzz", FizzBuzz.get(10));
+    }
+    @Test
+    @DisplayName("prints FizzBuzz")
+    public void testFizzbuzz(){
+        assertEquals("FizzBuzz", FizzBuzz.get(15));
+    }
+
 }
